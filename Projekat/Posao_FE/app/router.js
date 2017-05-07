6e00ddfaf677ebe8e8a5871b581bf1c55244f6ca
emir8gallo <b8emir@gmail.com>
2017-05-07 21:59:37 +0200

    Signed-off-by: emir8gallo <b8emir@gmail.com>

--- Projekat/Posao_FE/app/router.js
+++ Projekat/Posao_FE/app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('registracija');
+  this.route('registracijaNezaposleni');
+  this.route('registracijaPoslodavac');
 });
 
 export default Router;
