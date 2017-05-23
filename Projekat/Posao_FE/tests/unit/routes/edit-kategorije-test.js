5c8c04c99b4553352c6a74157fb87f1426285617
Haris Imamović <himamovic1@etf.unsa.ba>
2017-05-23 14:50:59 +0200

    Dodane stranice za pregled notifikacija, javnog profila i pregled i edit kategorija

---
+++ Projekat/Posao_FE/tests/unit/routes/edit-kategorije-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:/edit-kategorije', 'Unit | Route | /edit kategorije', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
