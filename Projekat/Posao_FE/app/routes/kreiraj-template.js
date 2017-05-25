2673ccbb4519498870cfd71c3ca2e05ee6db720e
edoimamovic <eimamovic1@etf.unsa.ba>
2017-05-25 21:03:21 +0200

    pregled korisnika postao pregled nezaposlenih

--- Projekat/Posao_FE/app/routes/kreiraj-template.js
+++ Projekat/Posao_FE/app/routes/kreiraj-template.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+	beforeModel: function(transition) {
+		//console.log(this.get('session.data.authenticated.role'));
+		//console.log(this.get('session.data.authenticated.role') !== "ROLE_POSLODAVAC");
+		//console.log((!this.get('session.isAuthenticated' || (this.get('session.data.authenticated.role') !== "ROLE_POSLODAVAC" ))));
+
+		if(!this.get('session.isAuthenticated') || (this.get('session.data.authenticated.role') !== "ROLE_ADMIN" )) {
+			return this.transitionTo("error");
+		}
+	},
+
 	model: function(params, transition){
 		let _template= {};
 		_template.naziv = "";
